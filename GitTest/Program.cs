c07033d4d2d06320b309dfb83742b39b4401d0be
SewerSnake <ericgroseclos@hotmail.com>
2018-11-03 11:04:23 +0100

    initial commit

---
+++ GitTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GitTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Write("Hello!");
+        }
+
+        static void Write(String text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
